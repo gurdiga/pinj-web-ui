c7b258eca397499db31a84a3bcf9fb3032680b92
Vlad GURDIGA <gurdiga@gmail.com>
2014-11-29 15:18:27 +0200

    make compile: fail on server errors

--- makefiles/deploy/compile.mk
+++ makefiles/deploy/compile.mk
@@ -4,14 +4,14 @@ compile-html:
 	@echo "Compipling HTML from templates:"
 	@coffee makefiles/common/page-template-pairs.coffee | \
 	while read html_file template; do \
-		wget --no-verbose http://localhost:$$HTTP_PORT/$$html_file --output-document=build/$$html_file; \
+		wget --no-verbose http://localhost:$$HTTP_PORT/$$html_file --output-document=build/$$html_file || exit 1; \
 	done
 
 compile-js:
 	@echo "Compipling JS bundles:"
 	@coffee makefiles/common/bundle-entry-pairs.coffee | \
 	while read bundle entry; do \
-		wget --no-verbose http://localhost:$$HTTP_PORT$$bundle --output-document=build$$bundle; \
+		wget --no-verbose http://localhost:$$HTTP_PORT$$bundle --output-document=build$$bundle || exit 1; \
 	done
 
 copy-static-assets:
