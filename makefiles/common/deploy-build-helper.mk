e052c87294c3c67ba940b96c8e913ba493d8799f
Vlad GURDIGA <gurdiga@gmail.com>
2014-12-02 04:51:41 +0200

    temp

--- makefiles/common/deploy-build-helper.mk
+++ makefiles/common/deploy-build-helper.mk
@@ -2,11 +2,16 @@ define deploy-build
 	$(eval remote=$(strip $(1)))
 	$(eval host=$(strip $(2)))
 
+	$(eval revision=$(strip $(3)))
+	$(eval revision=$(if $(revision),$(revision),master))
+
 	$(eval directory="build")
+
+	@echo "Deploying $(revision) to $(remote)/$(host):"
 
 	echo $(host) > $(directory)/CNAME && \
 	git add --force -- $(directory) && \
 	git commit --no-verify --message="build" -- $(directory) && \
-	git push --force $(remote) $$(git subtree split --prefix $(directory) master):gh-pages && \
+	git push --force $(remote) $$(git subtree split --prefix $(directory) $(revision)):gh-pages && \
 	git reset --hard HEAD~1
 endef
