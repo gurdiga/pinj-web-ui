a44c0f35f2122bf97d66c3fcea7ebe5b09c11076
Vlad GURDIGA <gurdiga@gmail.com>
2014-12-02 04:09:16 +0200

    temp

--- makefiles/production/production.mk
+++ makefiles/production/production.mk
@@ -7,7 +7,14 @@ production: \
 	delete-build-symlinks \
 	deploy-production \
 	clean-build \
-	create-build-symlinks
+	create-build-symlinks \
+	create-deploy-tag
 
 deploy-production:
 	$(call deploy-build, $(PRODUCTION_GIT_REMOTE), $(PRODUCTION_HOST_NAME))
+
+create-deploy-tag:
+	git tag "production-deploy-$$(date +%Y%m%d-%H%M)"
+	git push --tags origin
+
+rollback-production:
