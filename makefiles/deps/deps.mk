fc72d0db6a4b7a4e2d28afa9c60b0d504dec2dd5
Vlad GURDIGA <gurdiga@gmail.com>
2014-12-02 06:16:52 +0200

    make deps: housekeeping

--- makefiles/deps/deps.mk
+++ makefiles/deps/deps.mk
@@ -1,8 +1,12 @@
-deps:
-	@( \
-		ln --force --symbolic makefiles/deps/package.json && \
-		npm prune && npm install && \
-		ln --force --symbolic makefiles/deps/bower.json && \
-		bower prune && bower install \
-	)
-	@cp --verbose bower_components/pure/pure-nr-min.css src/web-ui/pages/common/css/pure-nr-min.scss
+deps: install-npm-packages install-bower-packages
+
+install-npm-packages:
+	ln --force --symbolic makefiles/deps/package.json && \
+	npm prune && \
+	npm install
+
+install-bower-packages:
+	ln --force --symbolic makefiles/deps/bower.json && \
+	bower prune && \
+	bower install && \
+	cp --verbose bower_components/pure/pure-nr-min.css src/web-ui/pages/common/css/pure-nr-min.scss
