f7e8d3b68d609266ac78d15dd4d8a7db65857473
Vlad GURDIGA <gurdiga@gmail.com>
2015-01-10 12:33:56 +0200

    Navigation: tests for getTitleForPage

--- test/widgets/navigation.js
+++ test/widgets/navigation.js
@@ -14,4 +14,16 @@ describe('Navigation', function() {
       }).to.throw(/Unknown page class “MagicPage”/);
     });
   });
+
+  describe('getTitleForPage', function() {
+    it('returns the link text for a given page', function() {
+      expect(Navigation.getTitleForPage('IndexPage')).to.equal('Home');
+    });
+
+    it('throws for an unknown page', function() {
+      expect(function() {
+        Navigation.getTitleForPage('MagicPage');
+      }).to.throw(/Unknown page class “MagicPage”/);
+    });
+  });
 });
