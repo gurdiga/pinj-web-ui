183af021af7d2464e764703d3ca1c0024bbb4ff7
Vlad GURDIGA <gurdiga@gmail.com>
2015-01-11 17:32:31 +0200

    Housekeeping

--- makefiles/common/deploy-build.mk
+++ makefiles/common/deploy-build.mk
@@ -2,11 +2,11 @@ define deploy-build
 	$(eval remote=$(strip $(1)))
 	$(eval host=$(strip $(2)))
 
-	$(eval directory="build")
+	$(eval build_directory="build")
 
-	echo $(host) > $(directory)/CNAME && \
-	git add --force -- $(directory) && \
-	git commit --no-verify --message="Production deploy" -- $(directory) && \
-	git push --force $(remote) $$(git subtree split --prefix $(directory) HEAD):gh-pages && \
+	echo $(host) > $(build_directory)/CNAME && \
+	git add --force -- $(build_directory) && \
+	git commit --no-verify --message="Production deploy" -- $(build_directory) && \
+	git push --force $(remote) $$(git subtree split --prefix $(build_directory) HEAD):gh-pages && \
 	git reset --hard HEAD~1
 endef
