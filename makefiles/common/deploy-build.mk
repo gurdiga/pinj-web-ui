17aefd0a6c2c20de5e777b4f3ef27eabd32d8469
Vlad GURDIGA <gurdiga@gmail.com>
2015-01-11 17:42:16 +0200

    make deploy-build: quiet subtree splitting

--- makefiles/common/deploy-build.mk
+++ makefiles/common/deploy-build.mk
@@ -7,6 +7,6 @@ define deploy-build
 	echo $(host) > $(build_directory)/CNAME && \
 	git add --force -- $(build_directory) && \
 	git commit --no-verify --message="Production deploy" -- $(build_directory) && \
-	git push --force $(remote) $$(git subtree split --prefix $(build_directory) HEAD):gh-pages && \
+	git push --force $(remote) $$(git subtree split -q --prefix $(build_directory) HEAD):gh-pages && \
 	git reset --hard HEAD~1
 endef
