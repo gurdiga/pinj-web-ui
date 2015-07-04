7035073a7cf8e2ea61a8041651048c66c454cb96
Vlad GURDIGA <gurdiga@gmail.com>
2015-07-04 15:42:27 +0300

    ClientListFormValidator

--- app/pages/client-list/client-list-form-validator.js
+++ app/pages/client-list/client-list-form-validator.js
@@ -5,19 +5,82 @@ var inherits = require('inherits');
 
 inherits(ClientListFormValidator, FormValidator);
 
-var PUNCTUATION_RE = /\./;
+var VALID_CHARS = /^[-0-9a-zа-яăîşţâ \\n#]*$/i;
+var MIN_LENGTH = 4;
+var MAX_ITEMS = 30;
 
 function ClientListFormValidator() {
   FormValidator.call(this);
 
   this.getRequirements = function() {
     return [{
-      'message': 'Este recomandat să excludeţi semnele de punctuaţie',
+      'message': 'Este recomandat să folosiţi doar cifre, caractere româneşti şi ruse, şi “-”.',
       'fulfilled': function(data) {
-        return !PUNCTUATION_RE.test(data['client-list']);
+        return checkItems(data, function(item) {
+          return VALID_CHARS.test(item);
+        });
+      }
+    }, {
+      'message': 'Nume, denumire sau număr de dosar prea scurt: trebuie cel puţin ' + MIN_LENGTH + ' caractere.',
+      'fulfilled': function(data) {
+        return checkItems(data, function(item) {
+          return item.length >= MIN_LENGTH;
+        });
+      }
+    }, {
+      'message': '“#” poate fi doar la început de linie pentru a desemna un număr de dosar.',
+      'fulfilled': function(data) {
+        return checkItems(data, function(item) {
+          var hashPosition = item.indexOf('#');
+          var hasHash = hashPosition > -1;
+          var beginsWithHash = hashPosition === 0;
+
+          return !hasHash || beginsWithHash;
+        });
+      }
+    }, {
+      'message': 'Numerele de dosar nu ar trebui să conţină spaţii.',
+      'fulfilled': function(data) {
+        return checkItems(data, function(item) {
+          var hasHash = item.indexOf('#') > -1;
+          var hasSpaces = item.indexOf(' ') > -1;
+          return !(hasHash && hasSpaces);
+        });
+      }
+    }, {
+      'message': 'În cadrul la denumiri, nu trebuie să fie spaţii lîngă “-”.',
+      'fulfilled': function(data) {
+        return checkItems(data, function(item) {
+          var hasSpacedDash = item.indexOf(' - ') > -1;
+          return !hasSpacedDash;
+        });
+      }
+    }, {
+      'message': 'Puteţi avea maximum ' + MAX_ITEMS +' de itemi în listă.',
+      'fulfilled': function(data) {
+        return items(data).length <= MAX_ITEMS;
       }
     }];
   };
+}
+
+function checkItems(data, f) {
+  return items(data).every(f);
+}
+
+function items(data) {
+  return data['client-list']
+    .split('\n')
+    .filter(nonEmpty)
+    .map(normalizeSpace);
+}
+
+function nonEmpty(item) {
+  return item.trim().length > 0;
+}
+
+function normalizeSpace(item) {
+  return item.replace(/\s+/g, ' ').trim();
 }
 
 module.exports = ClientListFormValidator;
--- test/pages/client-list/client-list-form-validator.js
+++ test/pages/client-list/client-list-form-validator.js
@@ -8,15 +8,84 @@ describe('ClientListFormValidator', function() {
   });
 
   it('accepts an empty list', function() {
-    expect(validate('')).to.be.resolved;
+    expect(validate('')).to.be.fulfilled;
   });
 
-  it('does not accept dots', function() {
-    expect(validate('Some Name.')).to.be.rejectedWith(/Este recomandat să excludeţi semnele de punctuaţie/);
+  var charactersToReject = '.,;:?!%&'.split('');
+
+  charactersToReject.forEach(function(character) {
+    it('does not accept “' + character + '”', function() {
+      return expect(validate('Good name\nName' + character))
+      .to.be.rejectedWith(/Este recomandat să folosiţi doar/);
+    });
+  });
+
+  it('rejects items shorter than 4', function() {
+    return expect(validate('Bip')).to.be.rejectedWith(/prea scurt/);
+  });
+
+  it('rejects space-normalized items shorter than 4', function() {
+    return expect(validate('   B   i   ')).to.be.rejectedWith(/prea scurt/);
+  });
+
+  it('ignores empty items', function() {
+    return expect(validate('some-name\n\n\nother name')).to.be.fulfilled;
+  });
+
+  it('ignore space-only items', function() {
+    return expect(validate('some-name\n  \n      \nother name\n     ')).to.be.fulfilled;
+  });
+
+  it('accepts short dashes', function() {
+    return expect(validate('some-name')).to.be.fulfilled;
+  });
+
+  it('accepts Russian characters', function() {
+    return expect(validate('Кыржалы')).to.be.fulfilled;
+  });
+
+  it('accepts Romanian characters', function() {
+    return expect(validate('MărţiŞor')).to.be.fulfilled;
+  });
+
+  it('accepts # at the beginning', function() {
+    return expect(validate('#238723876428734682')).to.be.fulfilled;
+  });
+
+  it('rejects # in any place else than the beginning', function() {
+    return expect(validate('2387#23876428734682')).to.be.rejectedWith(/doar la început/);
+  });
+
+  it('rejects items beginning with # and containing spaces', function() {
+    return expect(validate('#238723 876428734682')).to.be.rejectedWith(/Numerele de dosar nu ar trebui să conţină spaţii/);
+  });
+
+  it('rejects a dash if it has spaces around it', function() {
+    return expect(validate('some - name')).to.be.rejectedWith(/În cadrul la denumiri, nu trebuie să fie spaţii lîngă “-”/);
+  });
+
+  it('accepts a list of 30 items', function() {
+    var items = getRandomItems(30).join('\n');
+    return expect(validate(items)).to.be.fulfilled;
+  });
+
+  it('rejects a list of more than 30 items', function() {
+    var items = getRandomItems(31).join('\n');
+    return expect(validate(items)).to.be.rejectedWith(/Puteţi avea maximum 30 de itemi în listă/);
   });
 
   function validate(clientList) {
     return validator.validate({'client-list': clientList});
+  }
+
+  function getRandomItems(count) {
+    var items = [];
+
+    for (var i = 0; i < count; i++) {
+      items.push('random item ' + Date.now());
+    }
+
+    return items;
   }
 });
 
